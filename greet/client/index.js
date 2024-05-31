f4ee1d4752619c6054352382ece6eaa518bcfba2
Khuram Zahid <khuramzahid@gmail.com>
2024-05-31 16:33:17 +0500

    GreetManyTimes implemented

--- greet/client/index.js
+++ greet/client/index.js
@@ -17,11 +17,24 @@ function doGreet(client) {
   });
 }
 
+function doGreetManyTimes(client) {
+  console.log('doGreetManyTimes was invoked');
+
+  const req = new GreetRequest()
+    .setFirstName('Clement');
+  const call = client.greetManyTimes(req);
+
+  call.on('data', (res) => {
+    console.log(`GreetManyTimes: ${res.getResult()}`);
+  });
+}
+
 function main() {
   const creds = grpc.ChannelCredentials.createInsecure();
   const client = new GreetServiceClient('localhost:50051', creds,);
 
-  doGreet(client);
+  // doGreet(client);
+  doGreetManyTimes(client);
 }
 
 main();
